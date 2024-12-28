70ef046bbe275eccf00a29e665fa8fa7c7ed2dc8
xuan <xuan@reainc.com>
2024-12-28 16:39:04 -0500

    S5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,9 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
 import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import './app/layout/style.css'
+import App from './app/layout/App'
 
 
 createRoot(document.getElementById('root')!).render(
